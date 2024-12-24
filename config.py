7d36b5ca4f38ccf36dc2aa41e4653109d9ed237f
hdhfhfdjfjjf <Hshdjejee.djdjdjdjf@outlook.com>
2024-12-24 17:29:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY JASVEER SINGH
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7793482904:AAEssMcZNp4bPzK5xctHcFQqx_K85kvJ1uc")
+BOT_TOKEN=("7501498072:AAGY9V0qzE3dM-uaC2XdZQQHzBBxfYmgTws")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("Jasveer_s")
+OWNER_USERNAME=("Brar0315_bot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6857146936")
+ADMIN_IDS=("6237152221")
 
 
 
