f7cc86aeaed9dc54fc99b04bc5deb8e161748705
hdhfhfdjfjjf <Hshdjejee.djdjdjdjf@outlook.com>
2024-12-24 17:27:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY JASVEER SINGH
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7793482904:AAEssMcZNp4bPzK5xctHcFQqx_K85kvJ1uc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Jasveer_s")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6857146936")
+
+
+
+
+
+
+
+  
